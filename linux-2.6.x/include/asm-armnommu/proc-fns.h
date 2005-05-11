e32254c13f6a8d16d0c457300e0033750aa3ccf6
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2005-05-11 13:34:25 +0000

    Add support for ARM946E

--- linux-2.6.x/include/asm-armnommu/proc-fns.h
+++ linux-2.6.x/include/asm-armnommu/proc-fns.h
@@ -99,6 +99,14 @@
 #   define CPU_NAME cpu_arm940
 #  endif
 # endif
+# ifdef CONFIG_CPU_ARM946E
+#  ifdef CPU_NAME
+#   undef  MULTI_CPU
+#   define MULTI_CPU
+#  else
+#   define CPU_NAME cpu_arm946
+#  endif
+# endif
 # ifdef CONFIG_CPU_ARM1020
 #  ifdef CPU_NAME
 #   undef  MULTI_CPU
