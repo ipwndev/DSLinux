68d1a14215174e2e59a4cf313b09cc4f5955626d
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2005-05-22 10:21:03 +0000

    Add Nintendo version of tux logo

--- trunk/linux-2.6.x/drivers/video/logo/logo.c
+++ trunk/linux-2.6.x/drivers/video/logo/logo.c
@@ -26,6 +26,7 @@ extern const struct linux_logo logo_linux_vga16;
 extern const struct linux_logo logo_linux_clut224;
 extern const struct linux_logo logo_dec_clut224;
 extern const struct linux_logo logo_mac_clut224;
+extern const struct linux_logo logo_nds_clut224;
 extern const struct linux_logo logo_parisc_clut224;
 extern const struct linux_logo logo_sgi_clut224;
 extern const struct linux_logo logo_sun_clut224;
@@ -74,6 +75,10 @@ const struct linux_logo *fb_find_logo(int depth)
 		/* Macintosh Linux logo on m68k */
 		if (MACH_IS_MAC)
 			logo = &logo_mac_clut224;
+#endif
+#ifdef CONFIG_LOGO_NDS_CLUT224
+		/* Nintendo Linux logo */
+		logo = &logo_nds_clut224;
 #endif
 #ifdef CONFIG_LOGO_PARISC_CLUT224
 		/* PA-RISC Linux logo */
