b458f9782be250d565cb7f47e134d4b6d3afde5d
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2005-10-20 19:19:04 +0000

    Add M3 and Supercard CF support

--- trunk/linux-2.6.x/drivers/ide/arm/ide_arm.c
+++ trunk/linux-2.6.x/drivers/ide/arm/ide_arm.c
@@ -30,6 +30,33 @@
 # define IDE_ARM_IRQ	IRQ_HARDDISK
 #endif
 
+#ifdef CONFIG_IDE_NDS_M3
+static void M3_Unlock( void )
+{
+	volatile u16 tmp ;
+	tmp = *(volatile u16 *)0x08000000 ;
+	tmp = *(volatile u16 *)0x08E00002 ;
+	tmp = *(volatile u16 *)0x0800000E ;
+	tmp = *(volatile u16 *)0x08801FFC ;
+	tmp = *(volatile u16 *)0x0800104A ;
+	tmp = *(volatile u16 *)0x08800612 ;
+	tmp = *(volatile u16 *)0x08000000 ;
+	tmp = *(volatile u16 *)0x08801B66 ;
+	tmp = *(volatile u16 *)0x08800006 ;
+	tmp = *(volatile u16 *)0x08000000 ;
+}
+#endif
+
+#ifdef CONFIG_IDE_NDS_SUPERCARD
+static void Supercard_Unlock( void )
+{
+	*(volatile u16 *)0x09fffffe = 0xa55a ;
+	*(volatile u16 *)0x09fffffe = 0xa55a ;
+	*(volatile u16 *)0x09fffffe = 0x3 ;
+	*(volatile u16 *)0x09fffffe = 0x3 ;
+}
+#endif
+
 void __init ide_arm_init(void)
 {
 	if (IDE_ARM_HOST) {
@@ -37,9 +64,19 @@ void __init ide_arm_init(void)
 		int i;
 
 		memset(&hw, 0, sizeof(hw));
+#ifdef CONFIG_IDE_NDS_SUPERCARD
+		Supercard_Unlock();
+#endif
+#ifdef CONFIG_IDE_NDS_M3
+		M3_Unlock();
 		for (i=0; i<8; i++)
-		       	hw.io_ports[i] = 0x09000000 + 0x20000*i;
+			hw.io_ports[i] = 0x08800000 + 0x20000*i;
+		hw.io_ports[8] = 0x080C0000; // control
+#else
+		for (i=0; i<8; i++)
+			hw.io_ports[i] = 0x09000000 + 0x20000*i;
 		hw.io_ports[8] = 0x098C0000; // control
+#endif
 		hw.irq = IRQ_CART;
 		ide_register_hw(&hw, NULL);
 	}
