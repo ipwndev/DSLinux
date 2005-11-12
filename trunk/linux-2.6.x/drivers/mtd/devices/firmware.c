4a0daa8f70ffd33e28c08998f9300c9e86b68f39
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2005-11-12 22:29:29 +0000

    Use shmemipc layer

--- trunk/linux-2.6.x/drivers/mtd/devices/firmware.c
+++ trunk/linux-2.6.x/drivers/mtd/devices/firmware.c
@@ -5,6 +5,7 @@
  * Copyright (c) Jochen Schaeuble <psionic@psionic.de>
  * 07/2003      rewritten by Joern Engel <joern@wh.fh-wedel.de>
  * 09/2005      modified for Firmware by Malcolm Parsons <malcolm.parsons@gmail.com>
+ * 11/2005	shmemipc stuff added by Stefan Sperling <stsp@stsp.in-berlin.de>
  *
  */
 
@@ -14,27 +15,63 @@
 #include <linux/list.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
+#include <asm/semaphore.h>
+#include <linux/wait.h>
 #include <linux/mtd/mtd.h>
+
+#include <asm/arch/shmemipc.h>
 
 #define ERROR(fmt, args...) printk(KERN_ERR "firmware: " fmt , ## args)
 
 static struct mtd_info *mtdinfo;
+static int firmware_data_read;
+static wait_queue_head_t wait_queue;
 
 int firmware_read(struct mtd_info *mtd, loff_t from, size_t len,
 		size_t *retlen, u_char *buf)
 {
-    u32 * address ;
 
 	if (from + len > mtd->size)
 		return -EINVAL;
 
-    /* TODO: replace this with shmipc stuff: */
-    address = requestFirmwareFromArm7( from, len );
+	if (len > SHMEMIPC_FIRMWARE_DATA_SIZE)
+		len = SHMEMIPC_FIRMWARE_DATA_SIZE;
 
-	memcpy(buf, address, len);
+	firmware_data_read = 0;
+
+	shmemipc_lock();
+	SHMEMIPC_BLOCK_ARM9->firmware.from = from;
+	SHMEMIPC_BLOCK_ARM9->firmware.len = len;
+	SHMEMIPC_BLOCK_ARM9->firmware.destination = buf;
+	shmemipc_unlock();
+	/* 'flush' is not semantically correct here.
+	 * We are actually requesting the arm7 to read firmware data
+	 * and send us a "flush complete" */
+	shmemipc_flush(SHMEMIPC_USER_FIRMWARE);
+
+	if(wait_event_interruptible(wait_queue, firmware_data_read)) {
+		return -ERESTART;
+	}
 
 	*retlen = len;
 	return 0;
+}
+
+static void firmware_shmemipc_isr(u8 type)
+{
+	switch(type) {
+		case SHMEMIPC_REQUEST_FLUSH:
+			break;
+		case SHMEMIPC_FLUSH_COMPLETE:
+			memcpy(SHMEMIPC_BLOCK_ARM9->firmware.destination,
+			    &SHMEMIPC_BLOCK_ARM9->firmware.data,
+			    SHMEMIPC_BLOCK_ARM9->firmware.len);
+			firmware_data_read = 1;
+			wake_up_interruptible(&wait_queue);
+			break;
+		default:
+			break;
+	}
 }
 
 static void unregister_devices(void)
@@ -75,9 +112,17 @@ out1:
 	return ret;
 }
 
+static struct shmemipc_cb callback = {
+	.user = SHMEMIPC_USER_FIRMWARE,
+	.handler.firmware_callback = firmware_shmemipc_isr
+};
+
 int __init init_firmware(void)
 {
+	firmware_data_read = 0;
 	register_device("firmware", 256 * 1024);
+	init_waitqueue_head(&wait_queue);
+	register_shmemipc_cb(&callback);
 	return 0;
 }
 
@@ -90,5 +135,5 @@ module_init(init_firmware);
 module_exit(cleanup_firmware);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Malcolm Parsons <malcolm.parsons@gmail.com>");
+MODULE_AUTHOR("Malcolm Parsons <malcolm.parsons@gmail.com> / Stefan Sperling <stsp@stsp.in-berlin.de");
 MODULE_DESCRIPTION("MTD driver for Firmware");
