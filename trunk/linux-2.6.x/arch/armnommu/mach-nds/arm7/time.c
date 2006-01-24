0706f0582068447b7ef10f9bbc0755be1f5d5d6a
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-01-24 22:32:58 +0000

    Cleanup

--- trunk/linux-2.6.x/arch/armnommu/mach-nds/arm7/time.c
+++ trunk/linux-2.6.x/arch/armnommu/mach-nds/arm7/time.c
@@ -18,93 +18,94 @@
 #include "asm/types.h"
 #include "linux/timex.h"
 
-extern void swiDelay( u32 duration );
+#include "time.h"
+#include "arm7.h"
 
 static void rtcTransaction(u8 * command, u32 commandLen, u8 * result, u32 resultLen)
 {
-  u8 bit;
-  u8 i;
+	u8 bit;
+	u8 i;
 
-  RTC_CR8 = CS_0 | SCK_1 | SIO_1;
-  swiDelay(RTC_DELAY);
-  RTC_CR8 = CS_1 | SCK_1 | SIO_1;
-  swiDelay(RTC_DELAY);
+	RTC_CR8 = CS_0 | SCK_1 | SIO_1;
+	swiDelay(RTC_DELAY);
+	RTC_CR8 = CS_1 | SCK_1 | SIO_1;
+	swiDelay(RTC_DELAY);
 
-  for(i=0; i < commandLen; i++){
-    for (bit = 0; bit < 8; bit++) {
-      RTC_CR8 = CS_1 | SCK_0 | SIO_out | (command[i]>>7);
-      swiDelay(RTC_DELAY);
+	for(i=0; i < commandLen; i++){
+		for (bit = 0; bit < 8; bit++) {
+			RTC_CR8 = CS_1 | SCK_0 | SIO_out | (command[i]>>7);
+			swiDelay(RTC_DELAY);
 
-      RTC_CR8 = CS_1 | SCK_1 | SIO_out | (command[i]>>7);
-      swiDelay(RTC_DELAY);
+			RTC_CR8 = CS_1 | SCK_1 | SIO_out | (command[i]>>7);
+			swiDelay(RTC_DELAY);
 
-      command[i] = command[i] << 1;
-    }
-  }
+			command[i] = command[i] << 1;
+		}
+	}
 
-  for(i=0; i < resultLen; i++) {
-    result[i] = 0;
-    for (bit = 0; bit < 8; bit++) {
-      RTC_CR8 = CS_1 | SCK_0;
-      swiDelay(RTC_DELAY);
+	for(i=0; i < resultLen; i++) {
+		result[i] = 0;
+		for (bit = 0; bit < 8; bit++) {
+			RTC_CR8 = CS_1 | SCK_0;
+			swiDelay(RTC_DELAY);
 
-      RTC_CR8 = CS_1 | SCK_1;
-      swiDelay(RTC_DELAY);
+			RTC_CR8 = CS_1 | SCK_1;
+			swiDelay(RTC_DELAY);
 
-      if (RTC_CR8 & SIO_in) result[i] |= (1 << bit);
-    }
-  }
+			if (RTC_CR8 & SIO_in) result[i] |= (1 << bit);
+		}
+	}
 
-  RTC_CR8 = CS_0 | SCK_1;
-  swiDelay(RTC_DELAY);
+	RTC_CR8 = CS_0 | SCK_1;
+	swiDelay(RTC_DELAY);
 }
 
 
 static u8 BCDToInt(u8 data) {
-  return((data & 0xF) + ((data & 0xF0)>>4)*10);
+	return((data & 0xF) + ((data & 0xF0)>>4)*10);
 }
 
 static u32 get_nds_seconds(u8 * time)
 {
-  u8 hours = 0;
-  u8 i;
+	u8 hours = 0;
+	u8 i;
 
-  hours = BCDToInt(time[4] & HOUR_MASK);
+	hours = BCDToInt(time[4] & HOUR_MASK);
 
-  if((time[4] & RTC_PM) && (hours < 12)) {
-      hours += 12;
-  }
+	if((time[4] & RTC_PM) && (hours < 12)) {
+		hours += 12;
+	}
 
-  for(i = 0; i < 7; i++) {
-      time[i] = BCDToInt(time[i]);
-  }
+	for(i = 0; i < 7; i++) {
+		time[i] = BCDToInt(time[i]);
+	}
 
 
-  return( mktime(time[0]+2000,
-                 time[1],
-                 time[2],
-                 hours,
-                 time[5],
-                 time[6]));
+	return( mktime(time[0]+2000,
+				time[1],
+				time[2],
+				hours,
+				time[5],
+				time[6]));
 
 }
 
 
 u32 nds_get_time7(void)
 {
-  u8 command;
-  u8 time[8];
-  unsigned int seconds;
+	u8 command;
+	u8 time[8];
+	unsigned int seconds;
 
-  command = READ_DATA_REG1;
-  rtcTransaction(&command, 1, &(time[1]), 7);
+	command = READ_DATA_REG1;
+	rtcTransaction(&command, 1, &(time[1]), 7);
 
-  command = READ_STATUS_REG1;
-  rtcTransaction(&command, 1, &(time[0]), 1);
+	command = READ_STATUS_REG1;
+	rtcTransaction(&command, 1, &(time[0]), 1);
 
-  seconds = get_nds_seconds(&(time[1]));
+	seconds = get_nds_seconds(&(time[1]));
 
-  return seconds;
+	return seconds;
 
 }
 
