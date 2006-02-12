6ef4901bb939cd7a95a8fba3aed6b1408b5464fe
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-02-12 23:01:21 +0000

    SPI defines

--- trunk/linux-2.6.x/arch/armnommu/mach-nds/arm7/spi.h
+++ trunk/linux-2.6.x/arch/armnommu/mach-nds/arm7/spi.h
@@ -1,7 +1,7 @@
 #ifndef SPI_H
 #define SPI_H
-u16 touchRead(u32 command);
-s32 readTouchValue(int measure, int retry , int range);
+u16 touch_read(u32 command);
+s32 touch_read_value(int measure, int retry , int range);
 u8 power_read(void);
 void power_write(u8 val);
 void read_firmware(u32 address, u8 * destination, int count);
