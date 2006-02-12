563e0ff22890d00f8faef9e665f20bed3a9c4a09
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-02-12 23:01:21 +0000

    SPI defines

--- linux-2.6.x/arch/armnommu/mach-nds/arm7/spi.c
+++ linux-2.6.x/arch/armnommu/mach-nds/arm7/spi.c
@@ -3,57 +3,71 @@
 #include "asm/arch/fifo.h"
 
 extern void swiDelay(u32 duration);
-extern void swiWaitForVBlank(void);
-
-#define DISP_SR		(*(volatile u16*)0x04000004)
-
-#define DISP_VBLANK_IRQ	(1 << 3)
 
 #define REG_SPI_CR      (*(volatile u16*)0x040001C0)
 #define REG_SPI_DATA    (*(volatile u16*)0x040001C2)
 
-#define REG_SPI_ENABLE   0x8000
-#define SPI_BUSY     0x80
+#define SPI_4MHZ	(0<<0)
+#define SPI_2MHZ	(1<<0)
+#define SPI_1MHZ	(2<<0)
+#define SPI_512KHZ	(3<<0)
+#define SPI_BUSY	(1<<7)
+#define SPI_POWER	(0<<8)
+#define SPI_FIRMWARE	(1<<8)
+#define SPI_TOUCH	(2<<8)
+#define SPI_8CLOCKS	(0<<10)
+#define SPI_16CLOCKS	(1<<10)
+#define SPI_SINGLE	(0<<11)
+#define SPI_CONTINUOUS	(1<<11)
+#define SPI_INTERRUPT   (1<<14)
+#define SPI_ENABLE	(1<<15)
 
-u16 touchRead(u32 command)
+#define POWER_READ      (1<<7)
+#define POWER_WRITE     (0<<7)
+#define POWER_REG(n)	(n)
+
+#define WAIT_FOR_NOT_BUSY() {while (REG_SPI_CR & SPI_BUSY) swiDelay(1);}
+
+static u16 touch_read(u32 command)
 {
 	u16 result;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+
+	WAIT_FOR_NOT_BUSY();
 
 	// Write the command and wait for it to complete
-	REG_SPI_CR = REG_SPI_ENABLE | 0xA01;
+	REG_SPI_CR = SPI_ENABLE | SPI_CONTINUOUS | SPI_TOUCH | SPI_8CLOCKS | SPI_2MHZ ;
 	REG_SPI_DATA = command;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	// Write the second command and clock in part of the data
 	REG_SPI_DATA = 0;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 	result = REG_SPI_DATA;
 
 	// Clock in the rest of the data (last transfer)
-	REG_SPI_CR = REG_SPI_ENABLE | 0x201;
+	REG_SPI_CR = SPI_ENABLE | SPI_SINGLE | SPI_TOUCH | SPI_8CLOCKS | SPI_2MHZ ;
 	REG_SPI_DATA = 0;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	// Return the result
-	return ((result & 0x7F) << 5) | (REG_SPI_DATA >> 3);
+	result = ((result & 0x7F) << 5) | (REG_SPI_DATA >> 3);
+
+	REG_SPI_CR = 0;
+
+	return result ;
 }
 
 // Code from devkitpro/libnds/source/arm7/touch.c
 //---------------------------------------------------------------------------------
-s32 readTouchValue(int measure, int retry , int range) {
-	//---------------------------------------------------------------------------------
+s32 touch_read_value(int measure, int retry , int range) {
+//---------------------------------------------------------------------------------
 	int i;
 	s32 this_value=0, this_range;
 
-	s32 last_value = touchRead(measure | 1);
+	s32 last_value = touch_read(measure | 1);
 
 	for ( i=0; i < retry; i++) {
-		this_value = touchRead(measure | 1);
+		this_value = touch_read(measure | 1);
 		if ((last_value - this_value) > 0)
 			this_range = last_value - this_value;
 		else
@@ -63,27 +77,23 @@ s32 readTouchValue(int measure, int retry , int range) {
 
 	if ( i == range) this_value = 0;
 	return this_value;
-
 }
 
 u8 power_read(void)
 {
 	u8 val;
 
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
-	REG_SPI_CR = REG_SPI_ENABLE | (1<<1) | (1<<11) ;
-	REG_SPI_DATA = 0x80;
+	REG_SPI_CR = SPI_ENABLE | SPI_CONTINUOUS | SPI_POWER | SPI_8CLOCKS | SPI_1MHZ ;
+	REG_SPI_DATA = POWER_READ | POWER_REG(0) ;
 
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
-	REG_SPI_CR = REG_SPI_ENABLE | (1<<1) ;
+	REG_SPI_CR = SPI_ENABLE | SPI_SINGLE | SPI_POWER | SPI_8CLOCKS | SPI_1MHZ ;
 	REG_SPI_DATA = 0x0;
 
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	val = REG_SPI_DATA ;
 
@@ -94,18 +104,16 @@ u8 power_read(void)
 
 void power_write( u8 val )
 {
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	// Write the command and wait for it to complete
-	REG_SPI_CR = REG_SPI_ENABLE | (1<<1) | (1<<11) ;
-	REG_SPI_DATA = 0x00;
+	REG_SPI_CR = SPI_ENABLE | SPI_CONTINUOUS | SPI_POWER | SPI_8CLOCKS | SPI_1MHZ ;
+	REG_SPI_DATA = POWER_WRITE | POWER_REG(0) ;
 
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	// Write the data
-	REG_SPI_CR = REG_SPI_ENABLE | (1<<1) ;
+	REG_SPI_CR = SPI_ENABLE | SPI_SINGLE | SPI_POWER | SPI_8CLOCKS | SPI_1MHZ ;
 	REG_SPI_DATA = val;
 
 	REG_SPI_CR = 0 ;
@@ -115,32 +123,26 @@ void read_firmware(u32 address, u8 * destination, int count)
 {
 	int i;
 
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
-	REG_SPI_CR = 0x8900;
+	REG_SPI_CR = SPI_ENABLE | SPI_CONTINUOUS | SPI_FIRMWARE | SPI_8CLOCKS | SPI_4MHZ ;
 	REG_SPI_DATA = 3;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	REG_SPI_DATA = (address >> 16) & 255;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	REG_SPI_DATA = (address >> 8) & 255;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	REG_SPI_DATA = (address) & 255;
-	while (REG_SPI_CR & SPI_BUSY)
-		swiDelay(1);
+	WAIT_FOR_NOT_BUSY();
 
 	for (i = 0; i < count; i++) {
 		REG_SPI_DATA = 0;
-		while (REG_SPI_CR & SPI_BUSY)
-			swiDelay(1);
+		WAIT_FOR_NOT_BUSY();
 
 		destination[i] = REG_SPI_DATA;
 	}
-	REG_SPI_CR = 0x0000;
+	REG_SPI_CR = 0;
 }
