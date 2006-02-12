4afb03db9df403afbdf2aaf2d3f0c0f19c6ec350
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-02-12 18:00:08 +0000

    Better power control

--- linux-2.6.x/arch/armnommu/mach-nds/arm7/spi.h
+++ linux-2.6.x/arch/armnommu/mach-nds/arm7/spi.h
@@ -2,6 +2,7 @@
 #define SPI_H
 u16 touchRead(u32 command);
 s32 readTouchValue(int measure, int retry , int range);
-void poweroff(void);
+u8 power_read(void);
+void power_write(u8 val);
 void read_firmware(u32 address, u8 * destination, int count);
 #endif
