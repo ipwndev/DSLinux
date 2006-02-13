d4dbd1219cab90b2ca78cff771df95e0ba35010c
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-02-13 22:56:29 +0000

    Lindent

--- linux-2.6.x/arch/armnommu/mach-nds/fifo.c
+++ linux-2.6.x/arch/armnommu/mach-nds/fifo.c
@@ -12,8 +12,8 @@
 
 #include <linux/input.h>
 #include <linux/module.h>
-#include <linux/init.h> 
-#include <linux/interrupt.h> 
+#include <linux/init.h>
+#include <linux/interrupt.h>
 
 #include <asm/semaphore.h>
 #include <asm/irq.h>
@@ -25,55 +25,58 @@
 #define REG_IPCFIFORECV (*(volatile u32*) 0x04100000)
 #define REG_IPCFIFOCNT  (*(volatile u16*) 0x04000184)
 
-struct fifo_cb *cbhead = NULL ;
+struct fifo_cb *cbhead = NULL;
 static DECLARE_MUTEX(cbhead_mtx);
 
-static irqreturn_t ndsfifo_interrupt(int irq, void *dev_id, struct pt_regs *regs)
+static irqreturn_t ndsfifo_interrupt(int irq, void *dev_id,
+				     struct pt_regs *regs)
 {
 	u32 data;
-	struct fifo_cb * cb;
+	struct fifo_cb *cb;
 
-	while ( ! ( REG_IPCFIFOCNT & (1<<8) ) )
-	{
+	while (!(REG_IPCFIFOCNT & (1 << 8))) {
 		data = REG_IPCFIFORECV;
-		for ( cb = cbhead ; cb ; cb = cb->next )
-		{
-			if ( cb->type == ( data & 0xf0000000 ) )
-			{
-				switch ( cb->type )
-				{
-					case FIFO_BUTTONS:
-						cb->handler.button_handler( data & 0xff ) ;
-						break ;
-					case FIFO_TOUCH:
-						cb->handler.touch_handler( (data & (1<<16)) >> 16, (data & (0xff<<8)) >> 8, data & 0xff ) ;
-						break;
-                    case FIFO_TIME:
-                        cb->handler.time_handler( data & 0xffffff ) ;
-                        break;
-					case FIFO_WIFI:
-						cb->handler.wifi_handler(
-						  (data >> 18) & 0x3f,
-						  (data >> 16) & 0x03,
-						  data & 0xffff);
-						break;
-					default:
-						break;
+		for (cb = cbhead; cb; cb = cb->next) {
+			if (cb->type == (data & 0xf0000000)) {
+				switch (cb->type) {
+				case FIFO_BUTTONS:
+					cb->handler.button_handler(data & 0xff);
+					break;
+				case FIFO_TOUCH:
+					cb->handler.
+					    touch_handler((data & (1 << 16)) >>
+							  16,
+							  (data & (0xff << 8))
+							  >> 8, data & 0xff);
+					break;
+				case FIFO_TIME:
+					cb->handler.
+					    time_handler(data & 0xffffff);
+					break;
+				case FIFO_WIFI:
+					cb->handler.wifi_handler((data >> 18) &
+								 0x3f,
+								 (data >> 16) &
+								 0x03,
+								 data & 0xffff);
+					break;
+				default:
+					break;
 				}
 			}
 		}
 	}
 
-	return IRQ_HANDLED ;
+	return IRQ_HANDLED;
 }
 
-int register_fifocb( struct fifo_cb *fifo_cb )
+int register_fifocb(struct fifo_cb *fifo_cb)
 {
 	down(&cbhead_mtx);
-	fifo_cb->next = cbhead ;
-	cbhead = fifo_cb ;
+	fifo_cb->next = cbhead;
+	cbhead = fifo_cb;
 	up(&cbhead_mtx);
-	return 0 ;
+	return 0;
 }
 
 int unregister_fifocb(struct fifo_cb *fifo_cb)
@@ -82,7 +85,7 @@ int unregister_fifocb(struct fifo_cb *fifo_cb)
 
 	down(&cbhead_mtx);
 	for (cb = cbhead; cb; cb = cb->next) {
-		if (cb->next == NULL) { /* fifo_cb not found in list */
+		if (cb->next == NULL) {	/* fifo_cb not found in list */
 			up(&cbhead_mtx);
 			return -EINVAL;
 		} else if (cb->next == fifo_cb) {
@@ -96,21 +99,20 @@ int unregister_fifocb(struct fifo_cb *fifo_cb)
 
 static int __init ndsfifo_init(void)
 {
-        if (request_irq(IRQ_RECV, ndsfifo_interrupt, 0, "fifo", NULL)) {
-                printk(KERN_ERR "fifo.c: Can't allocate irq %d\n", IRQ_RECV);
-                return -EBUSY;
-        }
+	if (request_irq(IRQ_RECV, ndsfifo_interrupt, 0, "fifo", NULL)) {
+		printk(KERN_ERR "fifo.c: Can't allocate irq %d\n", IRQ_RECV);
+		return -EBUSY;
+	}
 
-	REG_IPCFIFOCNT = (1<<15) | (1<<10) | (1<<3) | (1<<14) ;
+	REG_IPCFIFOCNT = (1 << 15) | (1 << 10) | (1 << 3) | (1 << 14);
 
 	return 0;
 }
 
 static void __exit ndsfifo_exit(void)
 {
-        free_irq(IRQ_RECV, ndsfifo_interrupt);
+	free_irq(IRQ_RECV, ndsfifo_interrupt);
 }
 
 module_init(ndsfifo_init);
 module_exit(ndsfifo_exit);
-
