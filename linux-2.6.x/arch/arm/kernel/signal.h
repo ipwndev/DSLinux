3e8c2e62959c4835aebe466126707eae14432ba0
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-02-20 19:26:01 +0000

    Move sigreturn code to ITCM

--- linux-2.6.x/arch/arm/kernel/signal.h
+++ linux-2.6.x/arch/arm/kernel/signal.h
@@ -7,6 +7,10 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
+#ifdef CONFIG_ARCH_NDS
+#define KERN_SIGRETURN_CODE	0x00000500
+#else
 #define KERN_SIGRETURN_CODE	0xffff0500
+#endif
 
 extern const unsigned long sigreturn_codes[4];
