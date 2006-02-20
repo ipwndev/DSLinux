192b673aed908b143f216e51503d5d04735dd8b2
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-02-20 22:01:50 +0000

    io ports for CF devices in GBA slot.  M3 Unlock Sequence

--- trunk/linux-2.6.x/drivers/ide/arm/ide_arm.c
+++ trunk/linux-2.6.x/drivers/ide/arm/ide_arm.c
@@ -30,14 +30,42 @@
 # define IDE_ARM_IRQ	IRQ_HARDDISK
 #endif
 
+
+#ifdef CONFIG_IDE_NDS_M3
+static void M3_Unlock( void )
+{
+	volatile u16 tmp ;
+	tmp = *(volatile u16 *)0x08000000 ;
+	tmp = *(volatile u16 *)0x08E00002 ;
+	tmp = *(volatile u16 *)0x0800000E ;
+	tmp = *(volatile u16 *)0x08801FFC ;
+	tmp = *(volatile u16 *)0x0800104A ;
+	tmp = *(volatile u16 *)0x08800612 ;
+	tmp = *(volatile u16 *)0x08000000 ;
+	tmp = *(volatile u16 *)0x08801B66 ;
+	tmp = *(volatile u16 *)0x08800006 ;
+	tmp = *(volatile u16 *)0x08000000 ;
+}
+#endif
+
 void __init ide_arm_init(void)
 {
 	if (IDE_ARM_HOST) {
 		hw_regs_t hw;
+		int i;
 
 		memset(&hw, 0, sizeof(hw));
-		ide_std_init_ports(&hw, IDE_ARM_IO, IDE_ARM_IO + 0x206);
-		hw.irq = IDE_ARM_IRQ;
+#ifdef CONFIG_IDE_NDS_M3
+		M3_Unlock();
+		for (i=0; i<8; i++)
+			hw.io_ports[i] = 0x08800000 + 0x20000*i;
+		hw.io_ports[8] = 0x080C0000; // control
+#else
+		for (i=0; i<8; i++)
+			hw.io_ports[i] = 0x09000000 + 0x20000*i;
+		hw.io_ports[8] = 0x098C0000; // control
+#endif
+		hw.irq = IRQ_CART;
 		ide_register_hw(&hw, NULL);
 	}
 }
