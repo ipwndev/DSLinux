4769967dee00db948efb368f64332aba1b9b9374
pepsiman <pepsiman@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-03-17 15:38:25 +0000

    Add usage

--- vendors/Nintendo/update_config.sh
+++ vendors/Nintendo/update_config.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Usage:
+# Update common/config.vendor with a GBAMP config.vendor then run this script
+
 for i in [D-Z]* ; do cp common/config.vendor $i ; if [ -e $i/vendorpatch ] ; then cd $i ; patch -p1 < vendorpatch ; cd - ; fi ; done
