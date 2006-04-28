f892710d5ecf4f3edd11c159da082d8d2940f90b
amadeus_ds <amadeus_ds@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-04-28 18:37:29 +0000

    some test changes

--- trunk/linux-2.6.x/arch/arm/mach-nds/soundtest.c
+++ trunk/linux-2.6.x/arch/arm/mach-nds/soundtest.c
@@ -13,8 +13,17 @@
 #include <linux/module.h>
 #include <linux/init.h> 
 #include <linux/delay.h>
+#include <linux/interrupt.h>
 
 #include <asm/arch/fifo.h>
+
+#define TIMER1_DATA	(*(volatile u16*)0x04000104)
+#define TIMER2_DATA	(*(volatile u16*)0x04000108)
+#define TIMER1_CR	(*(volatile u16*)0x04000106)
+#define TIMER2_CR	(*(volatile u16*)0x0400010A)
+#define TIMER_ENABLE	(1<<7)
+#define TIMER_IRQ_REQ	(1<<6)
+#define TIMER_CASCADE	(TIMER_ENABLE|(1<<2))
 
 #define SAMPLERATE 	44000
 
@@ -33,10 +42,26 @@ static const short sintab[SINSIZE] = {
 -19260, -17557, -15786, -13952, -12062, -10126, -8149, -6140, -4107, -2057
 };
 
-static short soundbuffer[SINSIZE*2];
+#define BUFSIZE 10000
+#define INTSIZE 500
+
+static short soundbuffer[BUFSIZE];
+
+struct memdescr {
+	volatile short *start;
+	int size;
+	int index;
+	int sinindex;
+};
+
+static struct memdescr snddescr_left;
+static struct memdescr snddescr_right;
 
 static void soundtest_initsound(void)
 {
+	TIMER1_CR = 0;
+	TIMER2_CR = 0;
+
 	/* Enable Power */
 	nds_fifo_send(FIFO_SOUND | FIFO_SOUND_POWER | 1);
 	/* Wait some time to power up */
@@ -56,33 +81,80 @@ static void soundtest_initsound(void)
 	    (((u32) soundbuffer) & 0xffffff));
 
 	nds_fifo_send(FIFO_SOUND | FIFO_SOUND_RATE | SAMPLERATE);
+
+	TIMER1_DATA = 0 - ((0x1000000 / SAMPLERATE)*2);
+	TIMER2_DATA = 0 - INTSIZE;
 }
 
 /* 1 = start, 0 = stop */
 static void soundtest_play(int start)
 {
 	nds_fifo_send(FIFO_SOUND | FIFO_SOUND_TRIGGER | start);
+
+	/* send a dummy command */
+	nds_fifo_send(FIFO_SOUND | FIFO_SOUND_CHANNELS | 2);
+
+	/* wait until FIFO is empty */
+	while (!(NDS_REG_IPCFIFOCNT & FIFO_EMPTY))
+		;
+
+	/* now we can start the timer and be shure that
+           the timer interrupt is not comming to early. */
+	TIMER1_CR = TIMER_ENABLE;
+	TIMER2_CR = TIMER_CASCADE | TIMER_IRQ_REQ;
 }
+
+static void soundtest_write(struct memdescr *descr, int samples, int step)
+{
+	for (; samples; samples--) {
+		descr->start[descr->index++] = sintab[descr->sinindex];
+		if (descr->index >= descr->size)
+			descr->index = 0;
+		descr->sinindex += step;
+		if (descr->sinindex >= SINSIZE)
+			descr->sinindex -= SINSIZE;
+ 	}
+}
+
+static irqreturn_t soundtest_interrupt(int irq, void *dev_id,
+				     struct pt_regs *regs)
+{
+	soundtest_write( &snddescr_left, INTSIZE, 1);
+	soundtest_write( &snddescr_right, INTSIZE, 2);
+
+	return IRQ_HANDLED;
+}
+
 
 static void soundtest_fill(void)
 {
-	int i;
+	snddescr_left.start = &soundbuffer[0];
+	snddescr_left.size  = BUFSIZE/2;
+	snddescr_left.index = 0;
+	snddescr_left.sinindex = 0;
 
-	for (i = 0; i < SINSIZE; i++) {
-		soundbuffer[i] = sintab[i];		
-	}
-	for (i = 0; i < (SINSIZE/2); i++) {
-		soundbuffer[i+SINSIZE] = sintab[i*2];		
-	}
-	for (i = 0; i < (SINSIZE/2); i++) {
-		soundbuffer[i+SINSIZE+(SINSIZE/2)] = sintab[i*2];		
-	}
+	snddescr_right.start = &soundbuffer[BUFSIZE/2];
+	snddescr_right.size  = BUFSIZE/2;
+	snddescr_right.index = 0;
+	snddescr_right.sinindex = 0;
+
+	soundtest_write( &snddescr_left, BUFSIZE/2, 1);
+	soundtest_write( &snddescr_right, BUFSIZE/2, 2);
 }
 
 static int __init soundtest_init(void)
 {
 	soundtest_initsound();
 	soundtest_fill();
+
+#if 0
+	/* use a high priority timer */
+	if (request_irq(IRQ_TC2, soundtest_interrupt, SA_INTERRUPT, "NDS sound", 0)) {
+		printk(KERN_ERR "cannot grab irq %d\n", IRQ_TC2);
+		return -EBUSY;
+	}
+#endif
+
 	soundtest_play(1);
 	return 0;
 }
