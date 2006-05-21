052c8ba1d4a0444e2999cac6079e92670e1e02b7
nobody <nobody@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-05-21 21:41:12 +0000

    This commit was manufactured by cvs2svn to create tag 'mathomatic_12_5_16'.

---
+++ tags/mathomatic_12_5_16/user/mathomatic/lib/mathomatic.h
@@ -0,0 +1,6 @@
+/* Function prototypes for the Mathomatic library. */
+
+int matho_init(void);
+int matho_process(char *input, char **outputp);
+int matho_parse(char *input, char **outputp);
+int clear_all(void);
