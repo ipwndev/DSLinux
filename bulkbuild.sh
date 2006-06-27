f72a550c6927fb0e6a472110ab63d39897f2c3ec
stsp <stsp@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-06-27 19:43:29 +0000

    Add bulk build script.

---
+++ bulkbuild.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Run this script to do a bulk build of DSLinux.
+# This will produce a set of builds for distribution.
+#
+# Default configurations will be used.
+# WARNING: Local configuration changes will be nuked!
+
+set -e
+
+mktopconfig() {
+	cat > .config <<EOF
+CONFIG_DEFAULTS_NINTENDO=y
+CONFIG_DEFAULTS_NINTENDO_$1=y
+CONFIG_DEFAULTS_KERNEL_2_6=y
+# CONFIG_DEFAULTS_LIBC_NONE is not set
+CONFIG_DEFAULTS_LIBC_UCLIBC=y
+CONFIG_DEFAULTS_OVERRIDE=y
+# CONFIG_DEFAULTS_KERNEL is not set
+# CONFIG_DEFAULTS_VENDOR is not set
+# CONFIG_DEFAULTS_VENDOR_UPDATE is not set
+CONFIG_VENDOR=Nintendo
+CONFIG_PRODUCT=$1
+CONFIG_LINUXDIR=linux-2.6.x
+CONFIG_LIBCDIR=uClibc
+CONFIG_LANGUAGE=
+EOF
+}
+
+OUTDIR=${OUTDIR:-"./bulkbuild"}
+
+for build in DSGBA DSMEM GBAMP GBAMP_EXT2 M3CF NDS SUPERCARDCF
+do
+	if [ "$build" = "GBAMP_EXT2" ]
+	then
+		if which fakeroot > /dev/null
+		then
+			fakeroot_cmd="`which fakeroot`"
+		else
+			echo
+			echo
+			echo "Not building $build - fakeroot not found"
+			echo
+			echo
+			sleep 3
+			continue
+		fi
+	else
+		fakeroot_cmd=""
+	fi
+		
+	mktopconfig $build && yes '' | make config && $fakeroot_cmd make
+
+	case $build in
+		DSGBA)
+			distfile=dslinux.ds.gba
+		;;
+		DSMEM)
+			distfile=dslinux-dsmem.tgz
+		;;
+		GBAMP)
+			distfile=dslinux-gbamp.tgz
+		;;
+		GBAMP_EXT2)
+			distfile=dslinux-gbamp-ext2.tgz
+		;;
+		M3CF)
+			distfile=dslinux-m3cf.tgz
+		;;
+		SUPERCARDCF)
+			distfile=dslinux-supercardcf.tgz
+		;;
+		NDS)
+			distfile=dslinux.nds
+		;;	
+	esac
+
+	[ -d $OUTDIR ] || mkdir -p $OUTDIR
+	cp -v -f ./images/$distfile $OUTDIR
+	(cd $OUTDIR && md5sum $distfile | tee $distfile.md5)
+done
