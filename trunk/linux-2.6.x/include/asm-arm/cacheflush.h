c6f985a4d28b7c7e9fd26b054894fc62fd1205d9
dslinux_amadeus <dslinux_amadeus@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-08-06 20:23:24 +0000

    use proper function names for dmac_ functions

--- trunk/linux-2.6.x/include/asm-arm/cacheflush.h
+++ trunk/linux-2.6.x/include/asm-arm/cacheflush.h
@@ -150,7 +150,7 @@
  *	DMA Cache Coherency
  *	===================
  *
- *	dma_inv_range(start, end)
+ *	dmac_inv_range(start, end)
  *
  *		Invalidate (discard) the specified virtual address range.
  *		May not write back any entries.  If 'start' or 'end'
@@ -159,13 +159,13 @@
  *		- start  - virtual start address
  *		- end    - virtual end address
  *
- *	dma_clean_range(start, end)
+ *	dmac_clean_range(start, end)
  *
  *		Clean (write back) the specified virtual address range.
  *		- start  - virtual start address
  *		- end    - virtual end address
  *
- *	dma_flush_range(start, end)
+ *	dmac_flush_range(start, end)
  *
  *		Clean and invalidate the specified virtual address range.
  *		- start  - virtual start address
