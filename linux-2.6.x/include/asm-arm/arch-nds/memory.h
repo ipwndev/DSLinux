b9cd1da1a553f240514ff35cbcdb9deaa29295b1
dslinux_amadeus <dslinux_amadeus@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-08-19 10:58:20 +0000

    Remove definitions which are already defaults

--- linux-2.6.x/include/asm-arm/arch-nds/memory.h
+++ linux-2.6.x/include/asm-arm/arch-nds/memory.h
@@ -16,15 +16,6 @@
 #define TASK_SIZE	(0x01a00000UL)
 #define TASK_SIZE_26	TASK_SIZE
 
-/*
- * This decides where the kernel will search for a free chunk of vm
- * space during mmap's.
- */
-
-#define PHYS_OFFSET	(CONFIG_DRAM_BASE)
-#define PAGE_OFFSET 	(PHYS_OFFSET)
-#define END_MEM     	(CONFIG_DRAM_BASE + CONFIG_DRAM_SIZE)
-
 #define __virt_to_phys(vpage) ((unsigned long) (vpage))
 #define __phys_to_virt(ppage) ((unsigned long) (ppage))
 #define __virt_to_bus(vpage) ((unsigned long) (vpage))
