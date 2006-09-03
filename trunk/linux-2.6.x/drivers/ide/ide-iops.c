ce851728047938ec6bce2111b142f8dc6dfb61bf
dslinux_amadeus <dslinux_amadeus@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-09-03 20:22:27 +0000

    First "switching" CF driver for Supercard CF.

--- trunk/linux-2.6.x/drivers/ide/ide-iops.c
+++ trunk/linux-2.6.x/drivers/ide/ide-iops.c
@@ -30,40 +30,6 @@
 #include <asm/uaccess.h>
 #include <asm/io.h>
 
-#ifdef CONFIG_IDE_NDS_SUPERCARD
-
-#define NDS_IME     (*(volatile u32*)0x04000208)
-
-#define unlock() supercard_unlock()
-#define lock() supercard_lock()
-
-void supercard_unlock( void )
-{
-       NDS_IME = 0;
-       *(volatile u16 *)0x09fffffe = 0xa55a ;
-       *(volatile u16 *)0x09fffffe = 0xa55a ;
-       *(volatile u16 *)0x09fffffe = 0x7 ;
-       *(volatile u16 *)0x09fffffe = 0x7 ;
-}
-
-void supercard_lock( void )
-{
-       *(volatile u16 *)0x09fffffe = 0xa55a ;
-       *(volatile u16 *)0x09fffffe = 0xa55a ;
-       *(volatile u16 *)0x09fffffe = 0x5 ;
-       *(volatile u16 *)0x09fffffe = 0x5 ;
-       NDS_IME = 1;
-}
-
-#else
-
-#define unlock()  /* nothing */
-#define lock()    /* nothing */
-
-#endif
-
-
-
 /*
  *	Conventional PIO operations for ATA devices
  */
@@ -71,89 +37,67 @@ void supercard_lock( void )
 static u8 ide_inb (unsigned long port)
 {
 	u8 val ;
-	unlock() ;
 	val = (u8) inb(port);
-	lock() ;
 	return val ;
 }
 
 static u16 ide_inw (unsigned long port)
 {
 	u16 val ;
-	unlock() ;
 	val = (u16) inw(port);
-	lock() ;
 	return val ;
 }
 
 static void ide_insw (unsigned long port, void *addr, u32 count)
 {
-	unlock();
 	insw(port, addr, count);
-	lock();
 }
 
 static u32 ide_inl (unsigned long port)
 {
 	u32 val ;
-	unlock() ;
 	val = (u32) inl(port);
-	lock() ;
 	return val ;
 
 }
 
 static void ide_insl (unsigned long port, void *addr, u32 count)
 {
-	unlock();
 	insl(port, addr, count);
-	lock();
 }
 
 static void ide_outb (u8 val, unsigned long port)
 {
 	/* We call outw here because the GameBoyAdvance slot of the 
 	 * Nintendo DS cannot write 8bits. It can only write 16bits. */
-	unlock() ;
 	outw(val, port);
-	lock() ;
 }
 
 static void ide_outbsync (ide_drive_t *drive, u8 addr, unsigned long port)
 {
 	/* We call outw here because the GameBoyAdvance slot of the 
 	 * Nintendo DS cannot write 8bits. It can only write 16bits. */
-	unlock() ;
 	outw(addr, port);
-	lock() ;
 }
 
 static void ide_outw (u16 val, unsigned long port)
 {
-	unlock();
 	outw(val, port);
-	lock();
 }
 
 static void ide_outsw (unsigned long port, void *addr, u32 count)
 {
-	unlock();
 	outsw(port, addr, count);
-	lock();
 }
 
 static void ide_outl (u32 val, unsigned long port)
 {
-	unlock();
 	outl(val, port);
-	lock();
 }
 
 static void ide_outsl (unsigned long port, void *addr, u32 count)
 {
-	unlock();
 	outsl(port, addr, count);
-	lock();
 }
 
 void default_hwif_iops (ide_hwif_t *hwif)
