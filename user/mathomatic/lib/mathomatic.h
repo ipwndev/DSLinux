04e4a4c62d2bbf1667714d965531dce8e3d435a9
stsp <stsp@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-12-20 13:07:53 +0000

    Moving wireless_tools_27 tag into tags/wireless_tools/

--- user/mathomatic/lib/mathomatic.h
+++
@@ -1,6 +0,0 @@
-/* Function prototypes for the Mathomatic library. */
-
-int matho_init(void);
-int matho_process(char *input, char **outputp);
-int matho_parse(char *input, char **outputp);
-int clear_all(void);
