7a8015ebcc44c75c345e3763c7956027b999356e
stsp <stsp@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-12-20 13:07:53 +0000

    Moving retawq_0_2_6b tag into tags/retawq/

--- user/pixil/packages/flash/flash/Kflash/saver_common/xautolock.h
+++
@@ -1,9 +0,0 @@
-
-typedef enum { IGNORE, FORCE_SAVE, FORCE_LOCK } CornerAction;
-
-void setCorners( const char * );
-void forceTimeout();
-void initAutoLock();
-void cleanupAutoLock();
-int  waitTimeout( int );
-
