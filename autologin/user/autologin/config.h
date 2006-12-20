cd1808f95f9f1854a0b364e72b59bf81781e3af7
stsp <stsp@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2006-12-20 13:07:47 +0000

    Moving autologin branch into branches/vendor/

---
+++ autologin/user/autologin/config.h
@@ -0,0 +1,35 @@
+/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define HAVE_PAM if you, well, have PAM. */
+#undef HAVE_PAM
+
+/* Define if you have the setegid function.  */
+#undef HAVE_SETEGID
+
+/* Define if you have the seteuid function.  */
+#undef HAVE_SETEUID
+
+/* Define if you have the setgid function.  */
+#undef HAVE_SETGID
+
+/* Define if you have the setregid function.  */
+#undef HAVE_SETREGID
+
+/* Define if you have the setreuid function.  */
+#undef HAVE_SETREUID
+
+/* Define if you have the setuid function.  */
+#undef HAVE_SETUID
+
+/* Define if you have the dl library (-ldl).  */
+#undef HAVE_LIBDL
+
+/* Name of package */
+#undef PACKAGE
+
+/* Version number of package */
+#undef VERSION
+
+/* be paranoid */
+#undef PARANOID
+
