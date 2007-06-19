b8ba0868895b417231157644a66151927eb97d90
stsp <stsp@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2007-06-19 00:49:52 +0000

    Include /usr/games in default PATH.

--- user/busybox/shell/msh.c
+++ user/busybox/shell/msh.c
@@ -871,7 +871,7 @@ extern int msh_main(int argc, char **argv)
 	path = lookup("PATH");
 	if (path->value == null) {
 		if (geteuid() == 0)
-			setval(path, "/sbin:/bin:/usr/sbin:/usr/bin:/sram:/home/bin");
+			setval(path, "/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/sram:/home/bin");
 		else
 			setval(path, "/bin:/usr/bin");
 	}
