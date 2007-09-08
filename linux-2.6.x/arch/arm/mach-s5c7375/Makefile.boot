d06bcd4da5237c7a36a1fff24006b2e96186dbef
dslinux_amadeus <dslinux_amadeus@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2007-09-08 18:45:52 +0000

    tagging gdb 6.6

--- linux-2.6.x/arch/arm/mach-s5c7375/Makefile.boot
+++
@@ -1,2 +0,0 @@
-   zreladdr-y	:= 0x00008000
-initrd_phys-y	:= 0x00300000
--- user/pixil/packages/dvdview/dvdview/src/demo.sh
+++
@@ -1,1 +0,0 @@
-/usr2/microwin/src/bin/nano-X $1 & /usr2/microwin/src/bin/nanowm & ./dvdview -X -L -s /usr2/ccjump.mpeg; sleep 10000
