40917a7897081b840d1801bc35fb40553c01eab5
dslinux_gpf <dslinux_gpf@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2007-10-10 23:12:06 +0000

    tagging openvpn 2.0.9

--- user/pixil/packages/dvdview/dvdview/acconfig.h
+++
@@ -1,8 +0,0 @@
-
-@BOTTOM@
-
-/* Define if MMX assembly code should be compiled. */
-#undef ENABLE_MMX
-
-/* Define if your processor allows unaligned memory accesses. */
-#undef UNALIGNED_MEMORYACCESS
