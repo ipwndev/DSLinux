4ab4dc489abb78d3d54d82a6862fa15dde44f0a5
dslinux_gpf <dslinux_gpf@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2007-10-17 22:06:29 +0000

    tagging libiconv 1.11

--- linux-2.6.x/include/asm-arm/arch-s3c44b0x/param.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ARCH_ASM_PARAM_H__
-#define __ARCH_ASM_PARAM_H__
-
-/* nothing for now */
-
-#endif /* __ARCH_ASM_PARAM_H__ */
--- user/mc/src/src/selcodepage.h
+++
@@ -1,6 +0,0 @@
-#include <config.h>
-
-#ifdef HAVE_CHARSETS
-int do_select_codepage (void);
-int select_charset( int current_charset, int seldisplay );
-#endif /* !HAVE_CHARSETS */
