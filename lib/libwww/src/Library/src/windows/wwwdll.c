a11b59c41a0164bb52e8ce8e032a7c15036c2aeb
dslinux_gpf <dslinux_gpf@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2008-04-04 21:13:02 +0000

    tagging libogg 1.1.3

--- lib/libwww/src/Library/src/windows/wwwdll.c
+++
@@ -1,9 +0,0 @@
-/* HTDLL - module to be included only when making wwwuitls.dll
- * purpose: provide instances of the following exported globals:
- * 	    PTTYPrint - 
- * 	    WWW_TraceFlag - 
- */
-
-typedef int (TTYPrint_t)(unsigned int target, const char* fmt, ...);
-TTYPrint_t* PTTYPrint;
-int WWW_TraceFlag = 0;	/* Global trace flag for ALL W3 code */
--- user/mc/src/vfs/names.h
+++
@@ -1,4 +0,0 @@
-int finduid (char *name);
-void finduname (char *name, int uid);
-int findgid (char *name);
-void findgname (char *name, int gid);
