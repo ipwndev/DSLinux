927959b05c24faa5adbb1d0afbc9a6b98c07b12f
dslinux_sonny_jim <dslinux_sonny_jim@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2008-04-14 00:52:02 +0000

    Tagging cpscan-1.2

---
+++ tags/cpscan/cpscan-1.2/cpscan.h
@@ -0,0 +1,12 @@
+/* bugs coments etc degrade16@hotmail.com
+   by degrade
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define say printf /* mesmo que printf */
+
