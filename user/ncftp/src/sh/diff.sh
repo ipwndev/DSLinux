4d813b7167a2c3fb913fec13fa63b44bfddb1473
dslinux_sonny_jim <dslinux_sonny_jim@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2008-04-20 15:51:54 +0000

    Tagging e2fsprogs-1.40.8

--- user/ncftp/src/sh/diff.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-exec diff -r -u -w -d --exclude=configure --exclude=Makefile.in --exclude=config.h.in --exclude='*.m4' --exclude=acconfig.h --exclude='manifest*' --exclude='*.html' --exclude='*.1' --exclude='*.rc' --exclude='*.wse' "$@"
