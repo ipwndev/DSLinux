acf93c11b8ff2a4b357c11188c80e4f3d38f2334
dslinux_sonny_jim <dslinux_sonny_jim@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2009-08-10 10:15:22 +0000

    Updating fbview to 1.03 as per http://www.dslinux.org/f0rums/viewtopic.php?p=3802

--- trunk/user/fbview/framebuffer.c
+++ trunk/user/fbview/framebuffer.c
@@ -1,4 +1,19 @@
-//#define debug
+/*
+ *  (C) Copyright 2008-2009 Kamil Kopec <kamil_kopec@poczta.onet.pl>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
 
 #include <linux/fb.h>
 #include <malloc.h>
@@ -9,33 +24,48 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-//#ifdef debug
 #include <stdio.h>
-//#endif
 
 #include "types.h"
 #include "framebuffer.h"
 
-// we don't use palette for 16-bit mode
-// for 8-bit palette is always 8
-// for 4-bit palette is 2
-// other pals are ridiculus
+/* we don't use palette for 16-bit mode
+ * for 8-bit palette is always 8
+ * for 4-bit palette is 2
+ * other pals are ridiculus */
 #define fb_palette_colors (1<<var_info.bits_per_pixel)
 
+/* variable represents screen memory */
 fb_pixel * screen;
+
 Uint32 buffer_size;
-Uint32 fb_line_size;		// line of fb in bytes
-Uint8 have_cmap = 0;		// palette allocated?
 
-Uint16 fb_xres, fb_yres;		// screen resolution
+/* line of fb in bytes */
+Uint32 fb_line_size;
 
+/* palette allocated? */
+Uint8 have_cmap = 0;
+
+/* screen resolution */
+Uint16 fb_xres, fb_yres;
+
+/** framebuffer device descriptor */
 Sint32 fb_dev;
-struct fb_var_screeninfo var_info;
-struct fb_fix_screeninfo fb_info;
-Uint8 pixelsize;
-struct palette old_palette;
-struct palette global_pal;
 
+/** struct with framebuffer informations */
+struct fb_var_screeninfo var_info;
+
+/** more framebuffer informations */
+struct fb_fix_screeninfo fb_info;
+
+/** pixelsize by default is 2 */
+Uint8 pixelsize = 2;
+
+/** old palette of framebuffer */
+struct palette old_palette;
+
+/** palette, that our application will use */
+struct palette global_pal;
 
 struct palette
 {
@@ -44,6 +74,10 @@ struct palette
 	unsigned short *blue;
 };
 
+
+/**
+ * Allocates memory for the palette.
+ */
 void alloc_palette(struct palette *pal)
 {
 	pal->red=malloc(sizeof(unsigned short)*fb_palette_colors);
@@ -55,7 +89,9 @@ void alloc_palette(struct palette *pal)
 	}
 }
 
-
+/**
+ * Clears memory allocated for palette.
+ */
 void free_palette(struct palette *pal)
 {
 	free(pal->red);
@@ -63,7 +99,9 @@ void free_palette(struct palette *pal)
 	free(pal->blue);
 }
 
-
+/**
+ * Sets current palette to one given by parameter.
+ */
 void set_palette(struct palette *pal)
 {
 	struct fb_cmap cmap;
@@ -106,7 +144,9 @@ void set_palette(struct palette *pal)
 	free(r);free(g);free(b);free(t);
 }
 
-
+/**
+ * Returns current allocated palette.
+ */
 void get_palette(struct palette *pal)
 {
 	struct fb_cmap cmap;
@@ -153,6 +193,9 @@ void get_palette(struct palette *pal)
  * 009 009 021
  * 255 085 255
  * 036 036 084
+ */
+/**
+ * Tries to generate the best palette for the current framebuffer mode.
  */
 static void generate_palette(struct palette *palette)
 {
@@ -207,20 +250,30 @@ static void generate_palette(struct palette *palette)
                         /* stuff it in both high and low byte */
                 }
 	}
-}
+};
 
+/**
+ * function causes that textcursor doesn't blink.
+ */
 inline void fb_hide_cursor(void)
 {
 	printf("\033[10000B\033[10000C\033[?25l");
 	fflush(stdout);
-}
+};
 
+/**
+ * function causes that textcursor starts to blink.
+ */
 inline void fb_show_cursor(void)
 {
 	printf("\033[10000D\033[?25h");
 	fflush(stdout);
-}
+};
 
+
+/*
+Function uses for debugging.
+gives info about framebuffer state.
 void fb_dump_info()
 {
 	printf ("xres: %d\n", var_info.xres);
@@ -232,15 +285,22 @@ void fb_dump_info()
 	printf ("bits_per_pixel: %d\n", var_info.bits_per_pixel);
 	printf ("grayscale: %d\n", var_info.grayscale);
 };
+*/
 
+/**
+ * Initializes the framebuffer.
+ * @return 0 if success, 1 if failure
+ */
 int fb_init()
 {
+	/* opening device file */
 	if (!(fb_dev = open("/dev/fb0", O_RDWR)))
 	{
 		printf ("Unable to read framebuffer device: /dev/fb0\n");
 		return 1;
 	};
 
+	/* reading screen informations */
 	if (ioctl (fb_dev, FBIOGET_VSCREENINFO, &var_info))
 	{
 		printf ("Unable to read screen info.\n");
@@ -259,9 +319,7 @@ int fb_init()
 */
 	var_info.xoffset = 0;
 	var_info.yoffset = 0;
-//	var_info.bits_per_pixel = 32;
-//	var_info.yres_virtual = var_info.yres * 2;
-//	if (ioctl (fb_dev, FBIOPAN_DISPLAY, &var_info))
+
 	if (ioctl (fb_dev, FBIOPUT_VSCREENINFO, &var_info))
 	{
 		printf ("Unable to change screen data\n");
@@ -290,7 +348,10 @@ int fb_init()
 			return 3;
 	};
 
+	/* buffer_size = sizeof(screen) */
 	buffer_size = var_info.xres * var_info.yres * var_info.bits_per_pixel / 8;
+
+	/* mmap screen for framebuffer device */
 	if (!(screen = (Uint8 *) mmap (NULL, buffer_size, PROT_READ | PROT_WRITE, MAP_SHARED, fb_dev, 0)))
 	{
 		printf ("Unable to map shared memory\n");
@@ -299,8 +360,8 @@ int fb_init()
 		return 4;
 	};
 
-	// do we need to allocate new palette?
-	if (/*fb_info.visual==FB_VISUAL_PSEUDOCOLOR && */var_info.bits_per_pixel < 1<<24)
+	/* do we need to allocate new palette? */
+	if (var_info.bits_per_pixel < 1<<24)
 	{
 		have_cmap=1;
 		alloc_palette(&old_palette);
@@ -311,55 +372,131 @@ int fb_init()
 		set_palette(&global_pal);
 	}
 
+	/* store resolutions data */
 	fb_xres = var_info.xres;
 	fb_yres = var_info.yres;
 	fb_line_size = var_info.xres * pixelsize;
 
-	#ifdef debug
-	fb_dump_info();
-	#else
+	/* hide console cursor */
 	fb_hide_cursor();
-	#endif
 
 	return 0;
 }
 
+/**
+ * Uninitializes the framebuffer
+ * @return always 0
+ */
 int fb_uninit()
 {
+	/* unmmap screen of framebuffer device */
 	munmap(screen, buffer_size);
-	if (have_cmap)
-	{
+
+	/* have we changed the palette? */
+	if (have_cmap) {
+		/* if yes then restore the old one */
 		set_palette(&old_palette);
 		free_palette(&old_palette);
 		free_palette(&global_pal);
 	}
 	close(fb_dev);
 
-	#ifndef debug
+	/* show console cursor */
 	fb_show_cursor();
-	#endif
 
 	return 0;
 }
 
+/**
+ * Set black colour to all pixels of the screen.
+ * @param screen pointer to pixmap with width and height same as screen.
+ * Usually is it variable screen.
+ */
 void fb_clear_screen(fb_pixel * screen)
 {
 	memset(screen, 0, buffer_size/*fb_xres*fb_yres*pixelsize*/);
 }
 
-inline void fb_setpixel(fb_pixel * dest, Uint8 R, Uint8 G, Uint8 B)
+/**
+ * Paints a part of image on the screen. Id image is smaller than screen, function paints entire image.
+ * @param fb_pixel pixmap of image to paint on the screen
+ * @param width width od image
+ * @param height height of image
+ * @param pos_x position of the image to paint. Pixel pointed by pos_x will be drawn in the top, left corner.
+ * @param pos_y position of the image to paint. Pixel pointed by pos_y will be drawn in the top, left corner.
+ */
+void fb_pixmap_render(fb_pixel * fb_pixmap, Uint16 width , Uint16 height, Uint16 pos_x, Uint16 pos_y)
+{
+	/* This function is invoked many, many times,
+	 * so it is written as fast, as it was possible
+	 */
+	Uint32 j;
+	Uint32 copy_mem_size;
+	Uint32 bmp_line_size;
+	Uint32 fb_line_size;
+	Uint16 max_j;
+	
+	fb_pixel * tmp_screen;
+	fb_pixel * tmp_bmp;
+	
+	bmp_line_size = width*pixelsize;
+	fb_line_size = fb_xres*pixelsize;
+	tmp_bmp = fb_pixmap + pos_y*bmp_line_size + pos_x*pixelsize - bmp_line_size;
+	tmp_screen = screen - fb_line_size;
+	copy_mem_size = (fb_xres > width ? bmp_line_size : fb_line_size);
+	max_j = (fb_yres > height ? height : fb_yres);
+	
+	for(j = 0; j <  max_j ;j++)
+		memcpy(tmp_screen+=fb_line_size, tmp_bmp+=bmp_line_size, copy_mem_size);
+}
+
+/**
+ * Sets colour of pixel on the pixmap.
+ * @param dest memory adress of pixel
+ * @param R red value [0; 255];
+ * @param G green value [0; 255];
+ * @param B blue value [0; 255];
+ */
+inline void fb_set_pixel(fb_pixel * dest, Uint8 R, Uint8 G, Uint8 B)
 {
 	switch (pixelsize) {
 	case 1:
 		*(dest) = 0xe0 & R | 0x1c & (G>>3) | 0x03 & (B>>6);
 		break;
 	case 2:
-		*(dest+1) = 0x80 | 0xec & (B>>1) | 0x03 & (G>>6);
-		*(dest) = 0xe0 & (G<<2) | 0x1F & (R>>3);
+		*(dest+1) = 0x80 | 0x7c & (B>>1) | 0x03 & (G>>6);
+		*(dest) = 0xe0 & (G<<2) | 0x1f & (R>>3);
 		break;
 	case 3: case 4:
 		*(dest) = B;
 		*(dest+1) = G;
 		*(dest+2) = R;
+	};
+}
+
+/**
+ * Reads colour of pixel and saves it to RGB variables.
+ * @param dest memory adress of pixel
+ * @param R red value [0; 255];
+ * @param G green value [0; 255];
+ * @param B blue value [0; 255];
+ */
+inline void fb_get_pixel(fb_pixel * dest, Uint8 * R, Uint8 * G, Uint8 * B)
+{
+	switch (pixelsize) {
+	case 1:
+		*R = *(dest) & 0xe0;
+		*G = (*(dest) & 0x1c) << 3;
+		*B = (*(dest) & 0x03) << 6;
+		break;
+	case 2:
+		*R = ((*dest) & 0x1f) << 3;
+		*G = (((*(dest+1)) & 0x03) << 6) | (((*dest) & 0xe0) >> 2);
+		*B = ((*(dest+1)) & 0x7c) << 1;
+		break;
+	case 3: case 4:
+		*B = *(dest);
+		*G = *(dest+1);
+		*R = *(dest+2);
 	};
 }
--- trunk/user/fbview/framebuffer.h
+++ trunk/user/fbview/framebuffer.h
@@ -1,30 +1,94 @@
+/*
+ *  (C) Copyright 2008-2009 Kamil Kopec <kamil_kopec@poczta.onet.pl>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef ds_framebuffer_h
 #define ds_framebuffer_h
 
 #include "types.h"
 
-// 4MB memery limit
+/* 4MB memery limit */
 #define MEMSIZE (1024*1024*4)
 
-// byte per pixel for FrameBuffer device
+/* byte per pixel for framebuffer device */
 #define FB_BPP (sizeof(fb_pixel)*8)
 
-#pragma pack(push)
-#pragma pack(1)
-
-#pragma pack(pop)
-
+/** Type of data represents pixels in framebuffer device. */
 typedef Uint8 fb_pixel;
 
+/** Pixmap of screen. Every change of this variable will be imediatly seen by user */
 extern fb_pixel * screen;
+
+/** Pixel size in bytes. Usually it is 1 or 2 */
 extern Uint8 pixelsize;
+
+/** Memory size of one row. This is fb_xres * pixelsize */
 extern Uint32 fb_line_size;
+
+/** Resolution of screen, in X axis */
 extern Uint16 fb_xres;
+
+/** Resolution of screen, in Y axis */
 extern Uint16 fb_yres;
 
+/**
+ * Initializes the framebuffer.
+ * @return 0 if success, 1 if failure
+ */
 extern int fb_init();
+
+/**
+ * Uninitializes the framebuffer
+ * @return always 0
+ */
 extern int fb_uninit();
-extern void fb_setpixel(fb_pixel * dest, Uint8 R, Uint8 G, Uint8 B);
+
+/**
+ * Set black colour to all pixels of the screen.
+ * @param screen pointer to pixmap with width and height same as screen.
+ * Usually is it variable screen.
+ */
 extern void fb_clear_screen(fb_pixel * screen);
+
+/**
+ * Paints a part of image on the screen. Id image is smaller than screen, function paints entire image.
+ * @param fb_pixel pixmap of image to paint on the screen
+ * @param width width od image
+ * @param height height of image
+ * @param pos_x position of the image to paint. Pixel pointed by pos_x will be drawn in the top, left corner.
+ * @param pos_y position of the image to paint. Pixel pointed by pos_y will be drawn in the top, left corner.
+ */
+extern void fb_pixmap_render(fb_pixel * fb_pixmap, Uint16 width , Uint16 height, Uint16 pos_x, Uint16 pos_y);
+
+/**
+ * Sets colour of pixel on the pixmap.
+ * @param dest memory adress of pixel
+ * @param R red value [0; 255];
+ * @param G green value [0; 255];
+ * @param B blue value [0; 255];
+ */
+extern inline void fb_set_pixel(fb_pixel * dest, Uint8 R, Uint8 G, Uint8 B);
+
+/**
+ * Reads colour of pixel and saves it to RGB variables.
+ * @param dest memory adress of pixel
+ * @param R red value [0; 255];
+ * @param G green value [0; 255];
+ * @param B blue value [0; 255];
+ */
+extern inline void fb_get_pixel(fb_pixel * dest, Uint8 * R, Uint8 * G, Uint8 * B);
 
 #endif
