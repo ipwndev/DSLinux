9de7938f2bb64a738f64c23a11f5372fa9889b02
dslinux_sonny_jim <dslinux_sonny_jim@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2009-08-10 10:15:22 +0000

    Updating fbview to 1.03 as per http://www.dslinux.org/f0rums/viewtopic.php?p=3802

--- user/fbview/keyboard.c
+++ user/fbview/keyboard.c
@@ -1,58 +1,81 @@
+/*
+ *  (C) Copyright 2008-2009 Kamil Kopec <kamil_kopec@poczta.onet.pl>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #include <stdio.h>
 #include <termios.h>
 #include <term.h>
 #include <curses.h>
 #include <unistd.h>
 
+/** old and new keyboard setting */
 static struct termios initial_settings, new_settings;
-static int peek_character = -1;
 
+/** read stream buffer for keyboard */
+fd_set kb_rfds;
+
+/** timeval, for select's timeout */
+struct timeval kb_tv = {0};
+
+/**
+ * Initializes keyboard.
+ */
 void init_keyboard()
 {
-    tcgetattr(0,&initial_settings);
-    new_settings = initial_settings;
-    new_settings.c_lflag &= ~ICANON;
-    new_settings.c_lflag &= ~ECHO;
-    new_settings.c_lflag &= ~ISIG;
-    new_settings.c_cc[VMIN] = 1;
-    new_settings.c_cc[VTIME] = 0;
-    tcsetattr(0, TCSANOW, &new_settings);
+	tcgetattr(0,&initial_settings);
+	new_settings = initial_settings;
+	new_settings.c_lflag &= ~ICANON;
+	new_settings.c_lflag &= ~ECHO;
+	
+	new_settings.c_lflag &= ~ISIG;
+	new_settings.c_cc[VMIN] = 1;
+	new_settings.c_cc[VTIME] = 0;
+	
+	tcsetattr(0, TCSANOW, &new_settings);
 }
 
+/**
+ * Uninitializes keyboard.
+ */
 void close_keyboard()
 {
-    tcsetattr(0, TCSANOW, &initial_settings);
+	tcsetattr(0, TCSANOW, &initial_settings);
 }
 
+/**
+ * Checks if user has pressed a button.
+ * @return 0 if not, 1 if he does.
+ */
 int kbhit()
 {
-    char ch;
-    int nread;
+	FD_SET (0, &kb_rfds);
 
-    if(peek_character != -1)
-        return 1;
-    new_settings.c_cc[VMIN]=0;
-    tcsetattr(0, TCSANOW, &new_settings);
-    nread = read(0,&ch,1);
-    new_settings.c_cc[VMIN]=1;
-    tcsetattr(0, TCSANOW, &new_settings);
-
-    if(nread == 1) {
-        peek_character = ch;
-        return 1;
-    }
-    return 0;
+	if (select(1, &kb_rfds, NULL, NULL, &kb_tv) > 0) return 1;
+	else return 0;
 }
 
+/**
+ * Returns the last pressed button by user.
+ * @return Button code, that user has pressed.
+ */
 int readch()
 {
-    char ch;
-
-    if(peek_character != -1) {
-        ch = peek_character;
-        peek_character = -1;
-        return ch;
-    }
-    read(0,&ch,1);
-    return ch;
+	char ch[3];
+	int nread;
+	
+	nread = read(0, &ch, 3);
+	return (nread == 1 ? ch[0] : ch[2]);
 }
--- user/fbview/types.h
+++ user/fbview/types.h
@@ -1,14 +1,35 @@
+/*
+ *  (C) Copyright 2008-2009 Kamil Kopec <kamil_kopec@poczta.onet.pl>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License Version 2 as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 #ifndef ds_types_h
 #define ds_types_h
 
-//#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
-//#endif
+#define HAVE_SYS_TYPES_H
+#define HAVE_CTYPE_H
 
-#ifdef HAVE_CTYPE_H
-//# include <ctype.h>
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
 #endif
 
+#ifdef HAVE_CTYPE_H
+# include <ctype.h>
+#endif
+
+/* Types are the same as in SDL library */
 typedef int8_t		Sint8;
 typedef u_int8_t	Uint8;
 typedef int16_t		Sint16;
@@ -26,8 +47,8 @@ typedef struct {
 } Uint64, Sint64;
 #endif
 
+/* Make sure the types really have the right sizes */
 /*
-// Make sure the types really have the right sizes
 #if (sizeof(Uint8) != 1) || (sizeof(Sint8) != 1)
 #error Precompilation error, please post this bug id:00001 to programmer.
 #endif
