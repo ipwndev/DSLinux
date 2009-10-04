fd2edccc610cd455994b7a2ffcf41c5496797f50
dslinux_ljmerino <dslinux_ljmerino@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2009-10-04 18:00:33 +0000

    tagging cmatrix-1.2a

--- user/imagemagick/src/utilities/tests/common.sh
+++
@@ -1,8 +0,0 @@
-SRCDIR=`dirname $0`
-SRCDIR=`cd $SRCDIR && pwd`
-TOPSRCDIR=`cd $srcdir && pwd`
-mkdir -p utilities/tests
-cd utilities/tests || exit 1
-MODEL_MIFF="${TOPSRCDIR}/Magick++/demo/model.miff"
-SMILE_MIFF="${TOPSRCDIR}/Magick++/demo/smile.miff"
-GENERIC_TTF="${TOPSRCDIR}/PerlMagick/demo/Generic.ttf"
