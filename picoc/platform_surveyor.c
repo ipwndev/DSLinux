adb9e9e035472886222944b324e7a8c6d3c45297
dslinux_ljmerino <dslinux_ljmerino@ee40c8e4-d140-46bc-8bdd-8ae3c7fe0c09>
2009-11-20 00:22:06 +0000

    picoc vendor branch

---
+++ picoc/platform_surveyor.c
@@ -0,0 +1,58 @@
+#include "picoc.h"
+
+
+/* deallocate any storage */
+void PlatformCleanup()
+{
+}
+
+/* get a line of interactive input */
+char *PlatformGetLine(char *Buf, int MaxLen)
+{
+    int ix;
+    char ch, *cp;
+    
+    ix = 0;
+    cp = Buf;
+    while (ix++ < MaxLen) {
+        ch = getch();
+        if (ch == 0x1B) { // ESC character - exit
+            printf("leaving picoC\n\r");
+            return NULL;
+        }
+        if (ch == '\n') {
+            *cp++ = '\n';  // if newline, send newline character followed by null
+            *cp = 0;
+            return Buf;
+        }
+        *cp++ = ch;
+        ix++;
+    }
+    return NULL;
+}
+
+/* write a character to the console */
+void PlatformPutc(unsigned char OutCh, union OutputStreamInfo *Stream)
+{
+    if (OutCh == '\n')
+        putchar('\r');
+        
+    putchar(OutCh);
+}
+
+/* read a character */
+int PlatformGetCharacter()
+{
+    return getch();
+}
+
+/* mark where to end the program for platforms which require this */
+int ExitBuf[41];
+
+/* exit the program */
+void PlatformExit()
+{
+    ExitBuf[40] = 1;
+    longjmp(ExitBuf, 1);
+}
+
---
+++ picoc/tests/07_function.c
@@ -0,0 +1,21 @@
+int myfunc(int x)
+{
+    return x * x;
+}
+
+printf("%d\n", myfunc(3));
+printf("%d\n", myfunc(4));
+
+void vfunc(int a)
+{
+    printf("a=%d\n", a);
+}
+
+vfunc(1234);
+
+void qfunc()
+{
+    printf("qfunc()\n");
+}
+
+qfunc();
